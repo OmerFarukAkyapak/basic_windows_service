7b51174c6995024345d051eb21a12754d958d4e2
OmerFarukAkyapak <56655714+OmerFarukAkyapak@users.noreply.github.com>
2023-04-15 03:28:58 +0300

    .

---
+++ WindowsServiceExample/Service1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.ServiceProcess;
+
+
+namespace WindowsServiceExample
+{
+
+    public partial class Service1 : ServiceBase
+    {
+
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            DosyaYaz("Service is running. " + DateTime.Now);
+        }
+
+        protected override void OnStop()
+        {
+            DosyaYaz("Service has stopped. " + DateTime.Now);
+        }
+
+        public void DosyaYaz(string txt)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + "/Logs";
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            string text = AppDomain.CurrentDomain.BaseDirectory + "/Logs/services.txt";
+            if (!File.Exists(text))
+            {
+                using (StreamWriter sw = File.CreateText(text))
+                {
+                    sw.WriteLine(text);
+                }
+            }
+            else
+            {
+                using (StreamWriter sw = File.AppendText(text))
+                {
+                    sw.WriteLine(text);
+                }
+            }
+
+        }
+    }
+}
